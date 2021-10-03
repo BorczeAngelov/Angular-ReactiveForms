5a69fdf60ed7ac40c1310941cbbafff0ebc83d0a
Borcze Angelov <Borcze.Angelov@yahoo.com>
2021-10-03 19:35:28 +0200

    adjust validation rules at runtime

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -19,6 +19,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: "",
+      notification: 'email',
       sendCatalog: true
     });
   }
@@ -35,4 +37,16 @@ export class CustomerComponent implements OnInit {
       sendCatalog: false
     });
   }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerFormModel.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    }
+    else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
+  }
+
 }
